0367be48080de6e79e5f6a9c2769ef8b05f926a9
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-09-01 15:09:27 +0800

    Fix type issue in nixos-module.nix

--- nixos-module.nix
+++ nixos-module.nix
@@ -59,7 +59,7 @@ in {
       (pkgs.linkFarm "quadlet-service-symlinks" (
         map (p: {
           name = "etc/systemd/user/${p._unitName}";
-          path = "/run/user/${containersOwnerUID}/systemd/generator/${p._unitName}";
+          path = "/run/user/${builtins.toString containersOwnerUID}/systemd/generator/${p._unitName}";
         }) allObjects))
     ];
     # Inject X-RestartIfChanged=${hash} for NixOS to detect changes.
